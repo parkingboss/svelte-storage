39f7ef13dd12c8cd49b75f1250a56c121b150f3e
Chris Pfohl <christopher.pfohl@gmail.com>
2020-02-05 11:38:44 -0500

    Stop supporting browser, not needed since svelte Must be build anyway

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
-function browser(input, outfile) {
-	return {
-		input,
-		output: {
-			name: 'svelte-storage',
-			file: outfile,
-			format: 'umd',
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-		],
-	};
-}
 function mod(input, output, type) {
 	return {
 		input: input,
@@ -32,8 +18,6 @@ function mod(input, output, type) {
 }
 
 export default [
-	browser('src/main.js', pkg.browser),
-	browser('src/main.legacy.js', pkg.legacyBrowser),
 	mod('src/main.js', pkg.module, 'esm'),
 	mod('src/main.js', pkg.main, 'cjs'),
 	mod('src/main.legacy.js', pkg.legacyModule, 'esm'),
